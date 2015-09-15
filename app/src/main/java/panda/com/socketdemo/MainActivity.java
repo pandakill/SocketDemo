1c26ac97b5868e5788af2f1f8371dddf55302f47
panda <306641225@qq.com>
2015-09-15 10:28:41 +0800

    更新HTTP请求头、和字节流读取方式
    Signed-off-by: panda <306641225@qq.com>

--- app/src/main/java/panda/com/socketdemo/MainActivity.java
+++ app/src/main/java/panda/com/socketdemo/MainActivity.java
@@ -5,18 +5,13 @@ import android.os.Handler;
 import android.os.Bundle;
 import android.os.Message;
 import android.util.Log;
-import android.widget.Toast;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
-import java.net.InetSocketAddress;
 import java.net.Socket;
-import java.net.SocketAddress;
 
 
 public class MainActivity extends Activity {
@@ -25,11 +20,9 @@ public class MainActivity extends Activity {
 
     private BufferedReader mReader;
     private PrintWriter mWriter;
-    private BufferedInputStream bis;
-    private InputStream in;
 
-    private final static String HOST = "180.97.33.107"; // 百度的ip地址 180.97.33.107
-    private final static int POST = 80; //百度的端口 80
+    private final static String HOST = "www.baidu.com"; // 百度的ip地址 180.97.33.107
+    private final static int POST = 443; //百度的端口 80
 
     private final static int CONNECT = 100; // 连接成功代码
     private final static int BOND    = 200; // 响应开始代码
@@ -43,7 +36,7 @@ public class MainActivity extends Activity {
                 Log.i("mHandler", "连接成功");
             }
             if (msg.what == BOND) {
-                mReadThread.start();
+//                mReadThread.start();
                 Log.i("mHandler", "响应开始");
             }
             if (msg.what == HANDLED) {
@@ -60,43 +53,49 @@ public class MainActivity extends Activity {
             Log.i("mThread", "socket连接线程开启");
             // 建立socket连接
             try {
-                mSocket = new Socket();
+                // 设置true,是将返回结果以字节流形式获取
+                mSocket = new Socket(HOST, POST);
 
-                SocketAddress address = new InetSocketAddress(HOST, POST);
-
-                mSocket.connect(address, 3000);
                 System.out.println("mThread:" + mSocket.getLocalAddress());
-
                 mWriter = new PrintWriter(new OutputStreamWriter(mSocket.getOutputStream()));
-                mReader = new BufferedReader(new InputStreamReader(mSocket.getInputStream()));
-                bis = new BufferedInputStream(mSocket.getInputStream());
-
-                in = mSocket.getInputStream();
 
                 // 拼装请求头
-                mWriter.println("GET HTTP/1.1");
+                mWriter.println("HTTP/1.1 ");
+                mWriter.println("Host: www.baidu.com");
                 mWriter.println("Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
                 mWriter.println("Accept-Language: zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3");
                 mWriter.println("Accept-Encoding: gzip, deflate");
+                mWriter.println("Connection: keep-alive");
+                mReader = new BufferedReader(new InputStreamReader(mSocket.getInputStream()));
 
                 mWriter.flush();
 
-                if (mSocket.isConnected()) {
-                    msg.what = CONNECT;
-                    Log.i("mThread", "连接成功--isConnected");
-                } else {
-                    Log.i("mThread", "连接失败--isConnected");
+                try {
+                    // 设定读取的字节数
+                    byte buffer[] = new byte[2048];
+                    boolean flag = true;
+//                    String str = mReader.readLine();
+//                    InputStream in = mSocket.getInputStream();
+//                    while (in.read(buffer) != -1) {
+//                        Log.i("mReadThread", "buffer:"+new String(buffer));
+//                    }
+//                    InputStreamReader reader = new InputStreamReader(in);
+//                    BufferedReader breader = new BufferedReader(reader);
+                    // 读取socket返回的字节流
+                    while (flag) {
+                        String str = mReader.readLine();
+                        if (str != null) {
+                            Log.i("mReadThread", str);
+                        }
+                        if (mReader.read() == -1) {
+                            flag =false;
+                        }
+                    }
+                    msg.what = HANDLED;
+                    mHandler.sendMessage(msg);
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-                if (mSocket.isBound()) {
-                    msg.what = BOND;
-                    Log.i("mThread", "isBound");
-                } else {
-                    Log.i("mThread", "isBond else");
-                }
-
-                // 线程发送消息至handler
-                mHandler.sendMessage(msg);
-
 
             } catch (IOException e) {
                 e.printStackTrace();
@@ -104,56 +103,6 @@ public class MainActivity extends Activity {
         }
     });
 
-    // 新建线程2,用于处理socket响应
-    private Thread mReadThread = new Thread(new Runnable() {
-        Message msg = new Message();
-        @Override
-        public void run() {
-            Log.i("mReadThread", "处理字节流线程开启");
-            try {
-                // 设定读取的字节数
-                byte buffer[] = new byte[512];
-
-                while (in.read(buffer) != -1) {
-                    Log.i("mReadThread", "buffer=" + new String(buffer));
-                }
-
-//                String line = mReader.readLine();
-//                Log.i("mReadThread", "read完毕");
-//                if (line != null) {
-//                    Log.i("mReadThread", line);
-//                } else {
-//                    Log.i("mReadThread", "line is null");
-//                }
-                msg.what = HANDLED;
-                mHandler.sendMessage(msg);
-
-                // TODO 其他字节流读取方法
-//                // 读取输入流
-//                if (in == null) {
-//                    Log.i("mReadThread", "in is null");
-//                }
-//                buffer = (byte) mReader.read();
-//                Log.i("mReadThread", "buffer.length=" + new String(String.valueOf(buffer)));
-
-//                String str = "null";
-//                while(true)
-//                {
-//                    int size=0;
-//                    byte[] tmp=new byte[5];
-//                    in.read(tmp);
-//                    size=tmp[3]*256+tmp[4];
-//                    byte buf[]=new byte[size];
-//                    in.read(buf);
-//                    str=new String(buf);
-//                    Log.i("mReadThread", "buffer.length=" + str);
-//                }
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
