55ac29c0b9a47d63c449d4a9128b0eba29831dd7
panda <306641225@qq.com>
2015-09-21 20:30:55 +0800

    在字节流缓冲读取时，即将http响应的header获取，并在后续写文件操作中进行修改（不过好像依旧没有解决到问题、、回去再研究看一下）
    Signed-off-by: panda <306641225@qq.com>

--- app/src/main/java/panda/com/socketdemo/thread/DownloadThread.java
+++ app/src/main/java/panda/com/socketdemo/thread/DownloadThread.java
@@ -9,6 +9,7 @@ import java.io.RandomAccessFile;
 import java.net.Socket;
 
 import panda.com.socketdemo.model.Uri;
+import panda.com.socketdemo.utils.ResponseUtil;
 
 /**
  * 下载的线程
@@ -24,7 +25,7 @@ public class DownloadThread extends Thread {
     private Uri mDownUri;       // 文件下载路径
     private int mDownLength;    // 已经下载的大小(字节长度)
     private DownloadRequest mDownRequest;
-    public boolean mFinish = false;    // 当前线程是否下载完毕
+    private boolean mFinish = false;    // 当前线程是否下载完毕
 
     public DownloadThread(DownloadRequest downRequest,int block, File saveFile, int threadId, Uri downUrl,
                           int downLength) {
@@ -43,7 +44,7 @@ public class DownloadThread extends Thread {
         if(mDownLength < mBlock) {
             try {
                 Socket socket = new Socket(mDownUri.getHost(), mDownUri.getPort());
-                socket.setSoTimeout(5000);
+//                socket.setSoTimeout(5000);
 
                 // 线程开始下载的位置
                 int startPos = mBlock * (mThreadID - 1) + mDownLength;
@@ -55,25 +56,63 @@ public class DownloadThread extends Thread {
                 InputStream inputStream = socket.getInputStream();
                 // 拼装请求头
                 writer.println("GET " + mDownUri.getUrl() + " HTTP/1.1\r");
-                writer.println("Accept: image/gif, image/jpeg, image/pjpeg, image/pjpeg, application/x-shockwave-flash, application/xaml+xml, application/vnd.ms-xpsdocument, application/x-ms-xbap, application/x-ms-application, application/vnd.ms-excel, application/vnd.ms-powerpoint, application/msword, */*\r");
-                writer.println("Accept-Language: zh-CN\r");
+                writer.println("Host: " + mDownUri.getHost());
+                writer.println("Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r");
+                writer.println("Accept-Language: zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3\r");
                 writer.println("Referer: " + mDownUri.toString() + "\r");
-                writer.println("Charset: UTF-8\r");
+//                writer.println("Charset: UTF-8\r");
                 writer.println("Range: bytes=" + startPos + "-" + endPos + "\r");
-                writer.println("Connection: Keep-Alive\r");
+                writer.println("Connection: keep-alive\r");
                 writer.println("\r");
 
                 writer.flush();
 
-                byte[] buffer = new byte[1024];
+                byte[] buffer = new byte[1024*1024];
                 int len;
                 RandomAccessFile accessFile = new RandomAccessFile(mSaveFile, "rwd");
+                int current = 0;
+                String header = null;
+                boolean isHeader = false;
+                int responseCode = -1;
+
                 accessFile.seek(startPos);
                 while ((len = inputStream.read(buffer)) != -1) {
-                    accessFile.write(buffer, 0, len);
-                    mDownLength += len;
-                    mDownRequest.update(this.mThreadID, len);
-                    mDownRequest.append(len);
+                    // 截取请求头、以及响应body
+                    for (int i = 0; i < buffer.length; i ++) {
+                        if (buffer[i] == '\r' && buffer[i+1] == '\n'
+                                && buffer[i+2] == '\r' && buffer[i+3] == '\n') {
+                            current = i;
+                            isHeader = true;
+                            break;
+                        }
+                    }
+                    // 获取字节流当中的响应头
+                    if (isHeader) {
+                        isHeader = false;
+                        header = new String(buffer, 0, current);
+                        Log.i("DownloadThread", "第" + mThreadID + "个线程的header:\n" + header);
+                        Log.i("DownloadThread", "第" + mThreadID + "个线程的current:\n" + current);
+                        ResponseUtil util = new ResponseUtil(header, ResponseUtil.TYPE_HEADER);
+                        responseCode = (int) util.getResponseCode()[1];
+                        if (responseCode == 206) {
+                            accessFile.write(buffer, current + 4, len);
+                            mDownLength += (len-(current+4));
+                            mDownRequest.update(this.mThreadID, len-(current+4));
+                        } else {
+                            Log.e("DownloadThread", "线程" + mThreadID + "出现HTTP请求错误,HTTP响应码为:" + responseCode);
+                        }
+                    } else {
+                        // 如果响应头已经存在,则改部分缓冲字节流为body
+                        if (header != null) {
+                            if (responseCode == 206) {
+                                accessFile.write(buffer, 0, len);
+                                mDownLength += len;
+                                mDownRequest.update(this.mThreadID, len);
+                            } else {
+                                Log.e("DownloadThread", "线程" + mThreadID + "出现HTTP请求错误,HTTP响应码为:" + responseCode);
+                            }
+                        }
+                    }
                 }
 
                 // 关闭文件流、输入输出流
